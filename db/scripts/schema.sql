e5aec75e97429869f67f73d015605a01eaaa3e8b
nike z <nikegreen@yandex.ru>
2022-03-27 14:12:53 +0500

    4. Схема таблицы Post [#1731]

--- db/scripts/schema.sql
+++ db/scripts/schema.sql
@@ -1,3 +1,11 @@
+create table if not exists post (
+    id serial primary key,
+    name varchar,
+    text varchar,
+    link varchar unique not null,
+    created timestamp
+);
+
 create table if not exists rabbit (
     id serial primary key,
     created_date timestamp
