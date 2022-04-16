5b50f3a842687c3e5f6fe37d628e792f959c5185
nike z <nikegreen@yandex.ru>
2022-04-16 16:43:37 +0500

    3. Шаблонизатор. [#855 #289636]

--- src/test/java/ru/job4j/template/SimpleGeneratorTest.java
+++ src/test/java/ru/job4j/template/SimpleGeneratorTest.java
@@ -21,20 +21,23 @@ public class SimpleGeneratorTest {
     public void when2KeyOk() {
         Generator generator = new SimpleGenerator();
         Map<String, String> map = Map.of("key1", "ключ 1", "key2", "ключ 2");
-        assertTrue("Второй ключ = ключ 2. Это ключ 1!".equals(generator.produce("Второй ключ = ${key2}. Это ${key1}!", map)));
+        assertTrue("Второй ключ = ключ 2. Это ключ 1!".equals(
+                generator.produce("Второй ключ = ${key2}. Это ${key1}!", map)));
     }
 
     @Test(expected = IllegalFormatFlagsException.class)
     public void when1KeyMisssing() {
         Generator generator = new SimpleGenerator();
         Map<String, String> map = Map.of("key1", "ключ 1");
-        assertTrue("Второй ключ = ключ 2. Это ключ 1!".equals(generator.produce("Второй ключ = ${key2}. Это ${key1}!", map)));
+        assertTrue("Второй ключ = ключ 2. Это ключ 1!".equals(
+                generator.produce("Второй ключ = ${key2}. Это ${key1}!", map)));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void when1KeyError() {
         Generator generator = new SimpleGenerator();
         Map<String, String> map = Map.of("key1", "ключ 1", "key2", "ключ 2");
-        assertTrue("Второй ключ = ключ 2. Это ключ 1!".equals(generator.produce("Второй ключ = ${key2}. Это {key1}!", map)));
+        assertTrue("Второй ключ = ключ 2. Это ключ 1!".equals(
+                generator.produce("Второй ключ = ${key2}. Это {key1}!", map)));
     }
 }
