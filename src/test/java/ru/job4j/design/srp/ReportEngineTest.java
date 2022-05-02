30fde12ea1e5b7839acbba28d0aa1608dd2988e8
nike z <nikegreen@yandex.ru>
2022-05-02 19:02:10 +0500

    1. Отчеты. [#850] добавил html вид таблицы + test

--- src/test/java/ru/job4j/design/srp/ReportEngineTest.java
+++ src/test/java/ru/job4j/design/srp/ReportEngineTest.java
@@ -34,14 +34,31 @@ public class ReportEngineTest {
         Employee worker = new Employee("Ivan", now, now, 100);
         store.add(worker);
         Report engine = new ReportEngine(store, reportBuilder);
-        StringBuilder expect = new StringBuilder()
-                .append("Name; Hired; Fired; Salary;")
+        StringBuilder expect = new StringBuilder();
+        expect.append("<html>").append(System.lineSeparator())
+                .append("<header>").append(System.lineSeparator())
+                .append("</header>").append(System.lineSeparator())
+                .append("<body>").append(System.lineSeparator())
+                .append("<table border=\"1\">").append(System.lineSeparator())
+                .append("<tr>").append(System.lineSeparator())
+                .append("<th>Name</th>").append(System.lineSeparator())
+                .append("<th>Hired</th>").append(System.lineSeparator())
+                .append("<th>Fired</th>").append(System.lineSeparator())
+                .append("<th>Salary</th>").append(System.lineSeparator())
+                .append("</tr>").append(System.lineSeparator())
+                .append("<tr>").append(System.lineSeparator())
+                .append("<td>").append(worker.getName()).append("</td>")
                 .append(System.lineSeparator())
-                .append(worker.getName()).append(";")
-                .append(worker.getHired()).append(";")
-                .append(worker.getFired()).append(";")
-                .append(worker.getSalary()).append(";")
-                .append(System.lineSeparator());
+                .append("<td>").append(worker.getHired()).append("</td>")
+                .append(System.lineSeparator())
+                .append("<td>").append(worker.getFired()).append("</td>")
+                .append(System.lineSeparator())
+                .append("<td>").append(worker.getSalary()).append("</td>")
+                .append(System.lineSeparator())
+                .append("</tr>").append(System.lineSeparator())
+                .append("</table>").append(System.lineSeparator())
+                .append("</body>").append(System.lineSeparator())
+                .append("</html>").append(System.lineSeparator());
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
 }
